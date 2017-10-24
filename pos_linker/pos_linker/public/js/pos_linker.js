9608e34014424ce207251a6bb586927e12c2d382
OLSA <sasaoluic@yahoo.com>
2017-10-24 13:13:07 +0200

    Update pos_linker.js

--- pos_linker/pos_linker/public/js/pos_linker.js
+++ pos_linker/pos_linker/public/js/pos_linker.js
@@ -3,26 +3,27 @@
 // support: http://olsa.me
 // version: 1.0
 // date: 10.2017.
-
-erpnext.pos.PointOfSale.prototype.make_payment_modal = function(){
-        this.payment = new Payment({
-                frm: this.frm,
-                events: {
-                        submit_form: () => {                                
-                                $.ajax({
-                                    type: "POST",
-                                    crossDomain: true,                                    
-                                    headers : {"Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8"},
-                                    url: "http://127.0.0.1/pos/index.php",                                   
-                                    data: JSON.stringify(this.frm.doc),
-                                    contentType: "application/json; charset=utf-8",
-                                    dataType: "json",
-                                    success: function( msg ) {
-                                    console.log( "OK" );
-                                    }
-                                });
-                                this.submit_sales_invoice();
-                        }
-                }
-        });       
-}
+erpnext.pos.PointOfSale.prototype.make_payment_modal = function () {
+    this.payment = new Payment({
+        frm: this.frm,
+        events: {
+            submit_form: () => {
+                $.ajax({
+                    type: "POST",
+                    crossDomain: true,
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+                    },
+                    url: "http://127.0.0.1/pos/index.php",
+                    data: JSON.stringify(this.frm.doc),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (msg) {
+                        console.log("OK");
+                    }
+                });
+                this.submit_sales_invoice();
+            }
+        }
+    });
+};
